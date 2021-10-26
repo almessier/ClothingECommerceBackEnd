ab08c2846a4377273bce9df12f76809828ad65a8
MichaelBHerman <michaelbherman@gmail.com>
2021-10-26 09:20:26 -0600

    Added Modelbuilder for products

--- eCommerceStarterCode/Data/ApplicationDbContext.cs
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -10,19 +10,23 @@ namespace eCommerceStarterCode.Data
         public ApplicationDbContext(DbContextOptions options)
             : base(options)
         {
+
+        }
             public DbSet<Product> Products { get; set; }
             public DbSet<Review> Reviews { get; set; }
             public DbSet<ShoppingCart> ShoppingCarts { get; set; }
             public DbSet<User> Users { get; set; }
-        }
-
-    }
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfiguration(new RolesConfiguration());
+            modelBuilder.Entity<Product>()
+                .HasData(
+                    new Product { Id = 1, Name = "Cool Hoodie", Price = 499, Category = "Hoodies", Description = "Our coolest hoodie, ever." }
+                );
         }
 
     }
